e1efcac9805182a169a7f4d958beed2bbda354ad
Jon Wright <jon@streamdal.com>
2023-11-18 17:04:17 -0800

    Updated website to new requirements, started making code more maintainable with proper sass variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-	var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-if (isSafari) {
-    document.body.classList.add('safari');
-}
+    if (isSafari) {
+        document.body.classList.add('safari');
+    }
 
-    window.addEventListener('scroll', function (e) {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 100) {
             document.querySelector("header").classList.add('is-scrolling');
         } else {
@@ -25,9 +25,28 @@ if (isSafari) {
 
     mobileMenuLinks.forEach(function (link) {
         link.addEventListener('click', function () {
-
             menu_btn.classList.remove('is-active');
             mobile_menu.classList.remove('is-active');
+        });
+    });
+
+    // New functionality for toggling child divs based on parent div clicks
+    document.getElementById('select1').classList.add('is-active'); // Initialize 'Select 1' as active
+
+    document.querySelectorAll('.select-div').forEach(selectDiv => {
+        selectDiv.addEventListener('click', function() {
+            // Remove active class and hide all children
+            document.querySelectorAll('.select-div').forEach(div => div.classList.remove('is-active'));
+            document.querySelectorAll('.child-div').forEach(div => {
+                div.style.display = 'none';
+                div.setAttribute('aria-hidden', 'true');
+            });
+    
+            // Add active class to clicked div and show corresponding child
+            this.classList.add('is-active');
+            const childId = 'child' + this.id.replace('select', '');
+            document.getElementById(childId).style.display = 'block';
+            document.getElementById(childId).setAttribute('aria-hidden', 'false');
         });
     });
 };
